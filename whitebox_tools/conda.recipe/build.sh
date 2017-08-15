e378003066da372c2ae2bcd90760ec459b623679
gbrener <gbrener@continuum.io>
2017-08-15 16:42:37 -0500

    Add conda recipe

---
+++ whitebox_tools/conda.recipe/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd $SRC_DIR/whitebox_tools
+$PYTHON build.py
+mkdir $CONDA_PREFIX/share/whitebox_tools
+cp -av $SRC_DIR/whitebox_tools/target/release $CONDA_PREFIX/share/whitebox_tools
+$PYTHON setup.py install
+
+ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
+DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
+mkdir -p $ACTIVATE_DIR $DEACTIVATE_DIR
+cat > $ACTIVATE_DIR/whitebox_tools-activate.sh <<EOF
+echo export WHITEBOX_TOOLS_BUILD=$CONDA_PREFIX/share/whitebox_tools
+export WHITEBOX_TOOLS_BUILD=$CONDA_PREFIX/share/whitebox_tools
+EOF
+cat > $DEACTIVATE_DIR/whitebox_tools-deactivate.sh <<EOF
+echo unset WHITEBOX_TOOLS_BUILD
+unset WHITEBOX_TOOLS_BUILD
+EOF
